e26b977a1cea7b0c04a256cb3f420386fd9538ee
brema026 <Nif_Zawtx@outlook.com>
2024-04-14 00:27:22 -0600

    Primer avance

--- demo/src/main/java/org/example/demo/App.java
+++ demo/src/main/java/org/example/demo/App.java
@@ -2,17 +2,23 @@ package org.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("App-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root, 1300, 800);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
+
+        stage.setTitle("Text Finder");
         stage.setScene(scene);
         stage.show();
     }
