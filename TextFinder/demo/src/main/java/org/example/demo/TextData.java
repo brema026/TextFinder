2720da4614ff9cad1b46db047d435bd119e196f5
a-alfaro19 <a.alfaro@estudiantec.cr>
2024-05-04 14:07:54 -0600

    Revert "Merge pull request #13 from brema026/aleBenavides-feature-FileChooser"
    This reverts commit b0d8a7eab3faab2e9a688f6fd637201d742e784d, reversing
    changes made to 828d3408dbe1c391774302280851377db218cc50.

--- TextFinder/demo/src/main/java/org/example/demo/TextData.java
+++ TextFinder/demo/src/main/java/org/example/demo/TextData.java
@@ -2,15 +2,29 @@ package org.example.demo;
 
 import dataStructures.SinglyLinkedList;
 
+/**
+ * Represents the text data stored on the AVLTree
+ */
 public class TextData {
-    String text;
-    SinglyLinkedList<Occurrence> occurrences = new SinglyLinkedList<>();
+    String text; // The stored text
+    SinglyLinkedList<Occurrence> occurrences = new SinglyLinkedList<>(); // The text occurrence on the library documents
 
+    /**
+     * TextData object constructor.
+     *
+     * @param text       The text to be stored.
+     * @param occurrence The occurrence of the text.
+     */
     public TextData(String text, Occurrence occurrence) {
         this.text = text;
         addOccurrence(occurrence);
     }
 
+    /**
+     * Adds an occurrence of the text.
+     *
+     * @param occurrence The occurrence to add.
+     */
     public void addOccurrence(Occurrence occurrence) {
         occurrences.add(occurrence);
     }
