38e457ee848c1f493da0015c2cede2c7b1de2f0a
jnzhng444 <144281798+jnzhng444@users.noreply.github.com>
2024-05-04 12:38:11 -0600

    Revert "Parsear texto"

--- TextFinder/demo/src/main/java/org/example/demo/FinderController.java
+++ TextFinder/demo/src/main/java/org/example/demo/FinderController.java
@@ -1,30 +1,19 @@
 package org.example.demo;
 
-import dataStructures.SinglyLinkedList;
-import javafx.event.ActionEvent;
-
 import dataStructures.AVLTree;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import java.io.File;
+
 import java.net.URL;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.ResourceBundle;
-import javafx.collections.ObservableList;
-import javafx.collections.FXCollections;
 
 public class FinderController implements Initializable {
-
     @FXML
     private Button findButton;
     @FXML
@@ -35,18 +24,8 @@ public class FinderController implements Initializable {
     private Button deleteFileButton;
     @FXML
     private TextField finderText;
-    @FXML
-    private ListView<String> libraryListView;
-    private LibraryManager libraryManager;
-    private TextAreaController textAreaController;
 
-    public FinderController() {
-        libraryManager = LibraryManager.getInstance();
-    }
-
-    @Override
     public void initialize(URL location, ResourceBundle resources) {
-
         // Create imageview with background image
         ImageView viewFind = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/find.png"))));
         viewFind.setFitWidth(25);
@@ -97,54 +76,4 @@ public class FinderController implements Initializable {
         }
     }
 
-    public void setTextAreaController(TextAreaController textAreaController) {
-        this.textAreaController = textAreaController;
-    }
-
-    @FXML
-    public void handleFileButton() {
-        Stage stage = (Stage) fileButton.getScene().getWindow();
-
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select File");
-        File selectedFile = fileChooser.showOpenDialog(stage);
-
-        if (selectedFile != null) {
-            try {
-                libraryManager.addFile(selectedFile);
-                System.out.println("Document List after adding file: " + libraryManager.getDocuments());
-                refreshListView();
-
-                // Parsear el contenido del archivo
-                String content = DocumentParser.parseDocument(selectedFile);
-
-                // Mostrar el contenido en el TextArea
-                if (textAreaController != null) {
-                    textAreaController.setContent(content);
-                } else {
-                    System.err.println("TextAreaController no ha sido inicializado.");
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void refreshListView() {
-        ObservableList<String> items = FXCollections.observableArrayList();
-
-        SinglyLinkedList<Document> documents = libraryManager.getDocuments();
-        System.out.println("Tamaño de la lista de documentos: " + documents.getSize());
-
-        for (int i = 0; i < documents.getSize(); i++) {
-            Document document = documents.get(i);
-            items.add(document.getFileName());
-        }
-
-        System.out.println("Items antes de establecerla en la ListView: " + items);
-
-        libraryListView.setItems(items);
-        libraryListView.refresh();
-    }
 }
--- TextFinder/demo/src/main/java/org/example/demo/TextAreaController.java
+++ TextFinder/demo/src/main/java/org/example/demo/TextAreaController.java
@@ -2,41 +2,15 @@ package org.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextArea;
-import javafx.scene.text.Font;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-
-
 
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class TextAreaController implements Initializable {
-
     @FXML
-
-    private TextArea textArea;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        textArea.setEditable(false);
-        // Configurar la fuente y el tamaño de texto
-        Font font = new Font("Arial", 12);
-        textArea.setFont(font);
-
-        // Configurar el espaciado de línea mediante CSS
-        textArea.setStyle("-fx-line-spacing: 1.5em;");
-
-        // Configurar el color del texto
-        textArea.setStyle("-fx-text-fill: black;");
-
-        // Configurar el TextArea para que no tenga bordes
-        textArea.setStyle("-fx-border-color: transparent;");
-
-        // Establecer la alineación del texto
-        textArea.setStyle("-fx-alignment: top-left;");
-
     public TextFlow textFlow;
     public void initialize(URL location, ResourceBundle resources){
         setText("Esto es una ", "palabra", " destacada.");
@@ -70,10 +44,6 @@ public class TextAreaController implements Initializable {
         highlightedText.getStyleClass().add("highlightedText");
 
         return highlightedText;
-
     }
 
-    public void setContent(String content) {
-        textArea.setText(content);
-    }
 }
